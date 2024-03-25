dfe7f1c49b8cf0ebc37e05a2db2340a3b5c18234
Ervey Perez <sistemas3@grupomunoz.mx>
2024-03-25 13:35:45 -0600

    Retrofit setup JSON To Kotlin Class plugin was installed for create the data class entrys for News API.

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.ui.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "03ae4ef76e8b478fb47a928ed62670a8"
+        const val BASE_URL = "http://newapi.org"
+    }
+}
