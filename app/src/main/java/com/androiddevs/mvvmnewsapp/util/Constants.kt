62bbd46b543130fd30ebe7e850d19c3749d182bf
Ervey Perez <sistemas3@grupomunoz.mx>
2024-03-27 16:58:54 -0600

    Searching news

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "03ae4ef76e8b478fb47a928ed62670a8"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
