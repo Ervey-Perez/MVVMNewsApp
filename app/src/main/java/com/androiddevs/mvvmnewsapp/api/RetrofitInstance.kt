bdead896b4f81f2e612783048a5942f516b02d62
Ervey Perez <sistemas3@grupomunoz.mx>
2024-03-27 13:31:21 -0600

    Handling Breaking News Response Also a preview to search for news

--- app/src/main/java/com/androiddevs/mvvmnewsapp/api/RetrofitInstance.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/api/RetrofitInstance.kt
@@ -1,10 +1,12 @@
 package com.androiddevs.mvvmnewsapp.api
 
 import com.androiddevs.mvvmnewsapp.util.Constants.Companion.BASE_URL
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
 
 class RetrofitInstance {
     companion object {
@@ -12,9 +14,14 @@ class RetrofitInstance {
         private val retrofit by lazy {
             val logging = HttpLoggingInterceptor()
             logging.setLevel(HttpLoggingInterceptor.Level.BODY)
-            val client = OkHttpClient.Builder().addInterceptor(logging).build()
-            Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
-                .client(client).build()
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
         }
 
         val api by lazy{
--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -3,6 +3,6 @@ package com.androiddevs.mvvmnewsapp.util
 class Constants {
     companion object {
         const val API_KEY = "03ae4ef76e8b478fb47a928ed62670a8"
-        const val BASE_URL = "http://newapi.org"
+        const val BASE_URL = "https://newsapi.org"
     }
 }
